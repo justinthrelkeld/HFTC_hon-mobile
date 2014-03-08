f88a9a843a2dd0a3edec466734a2011999a8fee3
Justin Threlkeld <justin.threlkeld@gmail.com>
2014-03-07 19:52:41 -0600

    improve oncanvas JS handler

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,9 +1,15 @@
 function onCanvas(){
-  $('#show-search').on('click', function(){
+  $('#show-search').on('click', function(e){
     $('body').toggleClass('show-search');
+    e.stopPropagation();
   });
 
-  $('#show-menu').on('click', function(){
+  $('#show-menu').on('click', function(e){
     $('body').toggleClass('show-menu');
+    e.stopPropagation();
+  });
+  $('#main').on('click', function(e){
+    $('body').removeClass('show-menu show-search');
+    e.stopPropagation();
   });
 }
